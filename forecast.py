b1a6e7966eb981cc010b5bafed2c41f544b596e5
Allihasan Jillani <Allihasan.J@50hertz.local>
2026-07-14 12:39:57 +0530

    Replace project

--- forecast.py
+++
@@ -1,7 +0,0 @@
-pixel_speed = 100.6
-
-future_5_min = pixel_speed * 300
-future_15_min = pixel_speed * 900
-
-print("5 Minute Displacement:", future_5_min, "pixels")
-print("15 Minute Displacement:", future_15_min, "pixels")
--- image_size.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-img_path = "dataset/1_cumulus/" + os.listdir("dataset/1_cumulus")[0]
-
-img = Image.open(img_path)
-
-print(img.size)
